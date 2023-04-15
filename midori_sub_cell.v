79497f7f187f81c53451445dba99eb611dfaecf6
xjh <934622645@qq.com>
2023-04-15 15:25:48 +0800

    [midori] fix midori_sub_cell

--- midori_sub_cell.v
+++ midori_sub_cell.v
@@ -27,209 +27,209 @@ wire [127:0] state_tmp;
 // 4,1,6,3,0,5,2,7
 assign state_tmp[7:0] = {
 sb1[{
-state[0+4],
-state[0+1],
-state[0+6],
-state[0+3]}],
+state[7-4],
+state[7-1],
+state[7-6],
+state[7-3]}],
 sb1[{
-state[0+0],
-state[0+5],
-state[0+2],
-state[0+7]}
+state[7-0],
+state[7-5],
+state[7-2],
+state[7-7]}
 ]};
 // 4,1,6,3,0,5,2,7
 assign state_s[7:0] = {
-state_tmp[0+4],
-state_tmp[0+1],
-state_tmp[0+6],
-state_tmp[0+3],
-state_tmp[0+0],
-state_tmp[0+5],
-state_tmp[0+2],
-state_tmp[0+7]
+state_tmp[7-4],
+state_tmp[7-1],
+state_tmp[7-6],
+state_tmp[7-3],
+state_tmp[7-0],
+state_tmp[7-5],
+state_tmp[7-2],
+state_tmp[7-7]
 };
 
 // sp2
 // 1,6,7,0,5,2,3,4
 assign state_tmp[15:8] = {
 sb1[{
-state[8+1],
-state[8+6],
-state[8+7],
-state[8+0]}],
+state[15-1],
+state[15-6],
+state[15-7],
+state[15-0]}],
 sb1[{
-state[8+5],
-state[8+2],
-state[8+3],
-state[8+4]}
+state[15-5],
+state[15-2],
+state[15-3],
+state[15-4]}
 ]};
 // 3,0,5,6,7,4,1,2
 assign state_s[15:8] = {
-state_tmp[8+3],
-state_tmp[8+0],
-state_tmp[8+5],
-state_tmp[8+6],
-state_tmp[8+7],
-state_tmp[8+4],
-state_tmp[8+1],
-state_tmp[8+2]
+state_tmp[15-3],
+state_tmp[15-0],
+state_tmp[15-5],
+state_tmp[15-6],
+state_tmp[15-7],
+state_tmp[15-4],
+state_tmp[15-1],
+state_tmp[15-2]
 };
 
 // sp3
 // 2,3,4,1,6,7,0,5
 assign state_tmp[23:16] = {
 sb1[{
-state[16+2],
-state[16+3],
-state[16+4],
-state[16+1]}],
+state[23-2],
+state[23-3],
+state[23-4],
+state[23-1]}],
 sb1[{
-state[16+6],
-state[16+7],
-state[16+0],
-state[16+5]}
+state[23-6],
+state[23-7],
+state[23-0],
+state[23-5]}
 ]};
 // 6,3,0,1,2,7,4,5
 assign state_s[23:16] = {
-state_tmp[16+6],
-state_tmp[16+2],
-state_tmp[16+0],
-state_tmp[16+1],
-state_tmp[16+2],
-state_tmp[16+7],
-state_tmp[16+4],
-state_tmp[16+5]
+state_tmp[23-6],
+state_tmp[23-3],
+state_tmp[23-0],
+state_tmp[23-1],
+state_tmp[23-2],
+state_tmp[23-7],
+state_tmp[23-4],
+state_tmp[23-5]
 };
 
 // sp4
 // 7,4,1,2,3,0,5,6
 assign state_tmp[31:24] = {
 sb1[{
-state[24+7],
-state[24+4],
-state[24+1],
-state[24+2]}
+state[31-7],
+state[31-4],
+state[31-1],
+state[31-2]}
 ],
 sb1[{
-state[24+3],
-state[24+0],
-state[24+5],
-state[24+6]}
+state[31-3],
+state[31-0],
+state[31-5],
+state[31-6]}
 ]};
 // 5,2,3,4,1,6,7,0
 assign state_s[31:24] = {
-state_tmp[24+5],
-state_tmp[24+2],
-state_tmp[24+3],
-state_tmp[24+4],
-state_tmp[24+1],
-state_tmp[24+6],
-state_tmp[24+7],
-state_tmp[24+0]
+state_tmp[31-5],
+state_tmp[31-2],
+state_tmp[31-3],
+state_tmp[31-4],
+state_tmp[31-1],
+state_tmp[31-6],
+state_tmp[31-7],
+state_tmp[31-0]
 };
 //**********************63-32*************************
 // sp1
 // 4,1,6,3,0,5,2,7
 assign state_tmp[39:32] = {
 sb1[{
-state[32+4],
-state[32+1],
-state[32+6],
-state[32+3]}],
+state[39-4],
+state[39-1],
+state[39-6],
+state[39-3]}],
 sb1[{
-state[32+0],
-state[32+5],
-state[32+2],
-state[32+7]}
+state[39-0],
+state[39-5],
+state[39-2],
+state[39-7]}
 ]};
 // 4,1,6,3,0,5,2,7
 assign state_s[39:32] = {
-state_tmp[32+4],
-state_tmp[32+1],
-state_tmp[32+6],
-state_tmp[32+3],
-state_tmp[32+0],
-state_tmp[32+5],
-state_tmp[32+2],
-state_tmp[32+7]
+state_tmp[39-4],
+state_tmp[39-1],
+state_tmp[39-6],
+state_tmp[39-3],
+state_tmp[39-0],
+state_tmp[39-5],
+state_tmp[39-2],
+state_tmp[39-7]
 };
 
 // sp2
 // 1,6,7,0,5,2,3,4
 assign state_tmp[47:40] = {
 sb1[{
-state[40+1],
-state[40+6],
-state[40+7],
-state[40+0]}],
+state[47-1],
+state[47-6],
+state[47-7],
+state[47-0]}],
 sb1[{
-state[40+5],
-state[40+2],
-state[40+3],
-state[40+4]}
+state[47-5],
+state[47-2],
+state[47-3],
+state[47-4]}
 ]};
 // 3,0,5,6,7,4,1,2
 assign state_s[47:40] = {
-state_tmp[40+3],
-state_tmp[40+0],
-state_tmp[40+5],
-state_tmp[40+6],
-state_tmp[40+7],
-state_tmp[40+4],
-state_tmp[40+1],
-state_tmp[40+2]
+state_tmp[47-3],
+state_tmp[47-0],
+state_tmp[47-5],
+state_tmp[47-6],
+state_tmp[47-7],
+state_tmp[47-4],
+state_tmp[47-1],
+state_tmp[47-2]
 };
 
 // sp3
 // 2,3,4,1,6,7,0,5
 assign state_tmp[55:48] = {
 sb1[{
-state[48+2],
-state[48+3],
-state[48+4],
-state[48+1]}],
+state[55-2],
+state[55-3],
+state[55-4],
+state[55-1]}],
 sb1[{
-state[48+6],
-state[48+7],
-state[48+0],
-state[48+5]}
+state[55-6],
+state[55-7],
+state[55-0],
+state[55-5]}
 ]};
 // 6,3,0,1,2,7,4,5
 assign state_s[55:48] = {
-state_tmp[48+6],
-state_tmp[48+2],
-state_tmp[48+0],
-state_tmp[48+1],
-state_tmp[48+2],
-state_tmp[48+7],
-state_tmp[48+4],
-state_tmp[48+5]
+state_tmp[55-6],
+state_tmp[55-3],
+state_tmp[55-0],
+state_tmp[55-1],
+state_tmp[55-2],
+state_tmp[55-7],
+state_tmp[55-4],
+state_tmp[55-5]
 };
 
 // sp4
 // 7,4,1,2,3,0,5,6
 assign state_tmp[63:56] = {
 sb1[{
-state[56+7],
-state[56+4],
-state[56+1],
-state[56+2]}],
+state[63-7],
+state[63-4],
+state[63-1],
+state[63-2]}],
 sb1[{
-state[56+3],
-state[56+0],
-state[56+5],
-state[56+6]}
+state[63-3],
+state[63-0],
+state[63-5],
+state[63-6]}
 ]};
 // 5,2,3,4,1,6,7,0
 assign state_s[63:56] = {
-state_tmp[56+5],
-state_tmp[56+2],
-state_tmp[56+3],
-state_tmp[56+4],
-state_tmp[56+1],
-state_tmp[56+6],
-state_tmp[56+7],
-state_tmp[56+0]
+state_tmp[63-5],
+state_tmp[63-2],
+state_tmp[63-3],
+state_tmp[63-4],
+state_tmp[63-1],
+state_tmp[63-6],
+state_tmp[63-7],
+state_tmp[63-0]
 };
 //**********************95-64*************************
 
@@ -237,208 +237,208 @@ state_tmp[56+0]
 // 4,1,6,3,0,5,2,7
 assign state_tmp[71:64] = {
 sb1[{
-state[64+4],
-state[64+1],
-state[64+6],
-state[64+3]}],
+state[71-4],
+state[71-1],
+state[71-6],
+state[71-3]}],
 sb1[{
-state[64+0],
-state[64+5],
-state[64+2],
-state[64+7]}
+state[71-0],
+state[71-5],
+state[71-2],
+state[71-7]}
 ]};
 // 4,1,6,3,0,5,2,7
 assign state_s[71:64] = {
-state_tmp[64+4],
-state_tmp[64+1],
-state_tmp[64+6],
-state_tmp[64+3],
-state_tmp[64+0],
-state_tmp[64+5],
-state_tmp[64+2],
-state_tmp[64+7]
+state_tmp[71-4],
+state_tmp[71-1],
+state_tmp[71-6],
+state_tmp[71-3],
+state_tmp[71-0],
+state_tmp[71-5],
+state_tmp[71-2],
+state_tmp[71-7]
 };
 
 // sp2
 // 1,6,7,0,5,2,3,4
 assign state_tmp[79:72] = {
 sb1[{
-state[72+1],
-state[72+6],
-state[72+7],
-state[72+0]}],
+state[79-1],
+state[79-6],
+state[79-7],
+state[79-0]}],
 sb1[{
-state[72+5],
-state[72+2],
-state[72+3],
-state[72+4]}
+state[79-5],
+state[79-2],
+state[79-3],
+state[79-4]}
 ]};
 // 3,0,5,6,7,4,1,2
 assign state_s[79:72] = {
-state_tmp[72+3],
-state_tmp[72+0],
-state_tmp[72+5],
-state_tmp[72+6],
-state_tmp[72+7],
-state_tmp[72+4],
-state_tmp[72+1],
-state_tmp[72+2]
+state_tmp[79-3],
+state_tmp[79-0],
+state_tmp[79-5],
+state_tmp[79-6],
+state_tmp[79-7],
+state_tmp[79-4],
+state_tmp[79-1],
+state_tmp[79-2]
 };
 
 // sp3
 // 2,3,4,1,6,7,0,5
 assign state_tmp[87:80] = {
 sb1[{
-state[80+2],
-state[80+3],
-state[80+4],
-state[80+1]}],
+state[87-2],
+state[87-3],
+state[87-4],
+state[87-1]}],
 sb1[{
-state[80+6],
-state[80+7],
-state[80+0],
-state[80+5]}
+state[87-6],
+state[87-7],
+state[87-0],
+state[87-5]}
 ]};
 // 6,3,0,1,2,7,4,5
 assign state_s[87:80] = {
-state_tmp[80+6],
-state_tmp[80+2],
-state_tmp[80+0],
-state_tmp[80+1],
-state_tmp[80+2],
-state_tmp[80+7],
-state_tmp[80+4],
-state_tmp[80+5]
+state_tmp[87-6],
+state_tmp[87-3],
+state_tmp[87-0],
+state_tmp[87-1],
+state_tmp[87-2],
+state_tmp[87-7],
+state_tmp[87-4],
+state_tmp[87-5]
 };
 
 // sp4
 // 7,4,1,2,3,0,5,6
 assign state_tmp[95:88] = {
 sb1[{
-state[88+7],
-state[88+4],
-state[88+1],
-state[88+2]}],
+state[95-7],
+state[95-4],
+state[95-1],
+state[95-2]}],
 sb1[{
-state[88+3],
-state[88+0],
-state[88+5],
-state[88+6]}
+state[95-3],
+state[95-0],
+state[95-5],
+state[95-6]}
 ]};
 // 5,2,3,4,1,6,7,0
 assign state_s[95:88] = {
-state_tmp[88+5],
-state_tmp[88+2],
-state_tmp[88+3],
-state_tmp[88+4],
-state_tmp[88+1],
-state_tmp[88+6],
-state_tmp[88+7],
-state_tmp[88+0]
+state_tmp[95-5],
+state_tmp[95-2],
+state_tmp[95-3],
+state_tmp[95-4],
+state_tmp[95-1],
+state_tmp[95-6],
+state_tmp[95-7],
+state_tmp[95-0]
 };
 //************************127-96***********************
 // sp1
 // 4,1,6,3,0,5,2,7
 assign state_tmp[103:96] = {
 sb1[{
-state[96+4],
-state[96+1],
-state[96+6],
-state[96+3]}],
+state[103-4],
+state[103-1],
+state[103-6],
+state[103-3]}],
 sb1[{
-state[96+0],
-state[96+5],
-state[96+2],
-state[96+7]}
+state[103-0],
+state[103-5],
+state[103-2],
+state[103-7]}
 ]};
 // 4,1,6,3,0,5,2,7
 assign state_s[103:96] = {
-state_tmp[96+4],
-state_tmp[96+1],
-state_tmp[96+6],
-state_tmp[96+3],
-state_tmp[96+0],
-state_tmp[96+5],
-state_tmp[96+2],
-state_tmp[96+7]
+state_tmp[103-4],
+state_tmp[103-1],
+state_tmp[103-6],
+state_tmp[103-3],
+state_tmp[103-0],
+state_tmp[103-5],
+state_tmp[103-2],
+state_tmp[103-7]
 };
 
 // sp2
 // 1,6,7,0,5,2,3,4
 assign state_tmp[111:104] = {
 sb1[{
-state[104+1],
-state[104+6],
-state[104+7],
-state[104+0]}],
+state[111-1],
+state[111-6],
+state[111-7],
+state[111-0]}],
 sb1[{
-state[104+5],
-state[104+2],
-state[104+3],
-state[104+4]}
+state[111-5],
+state[111-2],
+state[111-3],
+state[111-4]}
 ]};
 // 3,0,5,6,7,4,1,2
 assign state_s[111:104] = {
-state_tmp[104+3],
-state_tmp[104+0],
-state_tmp[104+5],
-state_tmp[104+6],
-state_tmp[104+7],
-state_tmp[104+4],
-state_tmp[104+1],
-state_tmp[104+2]
+state_tmp[111-3],
+state_tmp[111-0],
+state_tmp[111-5],
+state_tmp[111-6],
+state_tmp[111-7],
+state_tmp[111-4],
+state_tmp[111-1],
+state_tmp[111-2]
 };
 
 // sp3
 // 2,3,4,1,6,7,0,5
 assign state_tmp[119:112] = {
 sb1[{
-state[112+2],
-state[112+3],
-state[112+4],
-state[112+1]}],
+state[119-2],
+state[119-3],
+state[119-4],
+state[119-1]}],
 sb1[{
-state[112+6],
-state[112+7],
-state[112+0],
-state[112+5]}
+state[119-6],
+state[119-7],
+state[119-0],
+state[119-5]}
 ]};
 // 6,3,0,1,2,7,4,5
 assign state_s[119:112] = {
-state_tmp[112+6],
-state_tmp[112+2],
-state_tmp[112+0],
-state_tmp[112+1],
-state_tmp[112+2],
-state_tmp[112+7],
-state_tmp[112+4],
-state_tmp[112+5]
+state_tmp[119-6],
+state_tmp[119-3],
+state_tmp[119-0],
+state_tmp[119-1],
+state_tmp[119-2],
+state_tmp[119-7],
+state_tmp[119-4],
+state_tmp[119-5]
 };
 
 // sp4
 // 7,4,1,2,3,0,5,6
 assign state_tmp[127:120] = {
 sb1[{
-state[120+7],
-state[120+4],
-state[120+1],
-state[120+2]}],
+state[127-7],
+state[127-4],
+state[127-1],
+state[127-2]}],
 sb1[{
-state[120+3],
-state[120+0],
-state[120+5],
-state[120+6]}
+state[127-3],
+state[127-0],
+state[127-5],
+state[127-6]}
 ]};
 // 5,2,3,4,1,6,7,0
 assign state_s[127:120] = {
-state_tmp[120+5],
-state_tmp[120+2],
-state_tmp[120+3],
-state_tmp[120+4],
-state_tmp[120+1],
-state_tmp[120+6],
-state_tmp[120+7],
-state_tmp[120+0]
+state_tmp[127-5],
+state_tmp[127-2],
+state_tmp[127-3],
+state_tmp[127-4],
+state_tmp[127-1],
+state_tmp[127-6],
+state_tmp[127-7],
+state_tmp[127-0]
 };
 //***********************************************
 
